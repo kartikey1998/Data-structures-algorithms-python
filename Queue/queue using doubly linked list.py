5260a9aa67a9e3d986ffc73c0a11ab8aae7eb556
Kartikey Chhipa <67624915+kartikey1998@users.noreply.github.com>
2021-06-02 17:49:19 +0530

    Add files via upload

---
+++ Queue/queue using doubly linked list.py
@@ -0,0 +1,65 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.last = None
+
+    def enqueue(self, data):
+        if self.last == None:
+            self.head = Node(data)
+            self.last = self.head
+
+        else:
+            self.last.next = Node(data)
+            self.last.next.prev = self.last
+            self.last = self.last.next
+
+    def dequeue(self):
+        if self.head == None:
+            return
+        else:
+            to_return = self.head.data
+            self.head = self.head.next
+            return to_return
+
+    def get_size(self):
+        itr = self.head
+        count = 0
+        while itr:
+            itr = itr.next
+            count +=1
+
+        return count
+
+    def print_queue(self):
+        itr = self.head
+        while itr:
+            print(itr.data, end='--->')
+            itr = itr.next
+        print('')
+
+
+que = Queue()
+que.enqueue(86)
+que.enqueue(68)
+que.enqueue(67)
+que.enqueue(98)
+que.enqueue(24)
+que.enqueue('kartikey')
+que.print_queue()
+que.dequeue()
+que.print_queue()
+que.dequeue()
+que.dequeue()
+que.print_queue()
+print(que.dequeue())
+que.enqueue(2654)
+que.enqueue(184)
+que.enqueue(965)
+que.print_queue()
+print(que.get_size())
