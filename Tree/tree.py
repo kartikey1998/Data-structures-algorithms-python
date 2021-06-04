9c05298468c52b3040b243a025b58667d76a52b3
Kartikey Chhipa <67624915+kartikey1998@users.noreply.github.com>
2021-06-04 22:54:21 +0530

    Add files via upload

---
+++ Tree/tree.py
@@ -0,0 +1,46 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+        self.parent = None
+
+
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
+
+    def get_level(self):
+        level = 0
+        p = self.parent
+        while p:
+            p = p.parent
+            level +=1
+        return level
+
+    def print_tree(self):
+        prefix = (' '*(self.get_level())*3 ) +'|___'
+        print(prefix,self.data)
+        if len(self.children)>0:
+            for child in self.children:
+                child.print_tree()
+
+root = TreeNode("Electronics")
+
+laptop = TreeNode("Laptop")
+laptop.add_child(TreeNode("Mac"))
+laptop.add_child(TreeNode("Surface"))
+laptop.add_child(TreeNode("Thinkpad"))
+
+cellphone = TreeNode("Cell Phone")
+cellphone.add_child(TreeNode("iPhone"))
+cellphone.add_child(TreeNode("Google Pixel"))
+cellphone.add_child(TreeNode("Vivo"))
+
+tv = TreeNode("TV")
+tv.add_child(TreeNode("Samsung"))
+tv.add_child(TreeNode("LG"))
+
+root.add_child(laptop)
+root.add_child(cellphone)
+root.add_child(tv)
+root.print_tree()
